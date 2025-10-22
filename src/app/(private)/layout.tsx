aef54d258fdc2f3b71ca824d41b487f3458ba114
somn45 <somnsomn45@gmail.com>
2025-10-22 17:10:54 +0900

    feat : 홈 페이지에 임시 네비게이션 추가

---
+++ src/app/(private)/layout.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+
+export default function ProtectedLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <Link href="/">메인으로</Link>
+            </li>
+            <li>
+              <Link href="/todos">투두리스트</Link>
+            </li>
+            <li>
+              <Link href="/login">로그인</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>{children}</main>
+    </>
+  );
+}
--- src/app/page.tsx
+++
@@ -1,6 +0,0 @@
-import { getMockData } from "@/apis/getMockData";
-
-export default async function Home() {
-  const mockData = await getMockData();
-  return <h1>Hello World</h1>;
-}
