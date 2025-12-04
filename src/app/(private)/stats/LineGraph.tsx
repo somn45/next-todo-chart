8ba97d0d71bc44672ca5b2312206f252ae4be0b9
somn45 <somnsomn45@gmail.com>
2025-12-04 23:35:37 +0900

    feat : 투두 라인 그래프의 라인 색상 수정 및 마우스 이벤트 추가

--- src/app/(private)/stats/LineGraph.tsx
+++ src/app/(private)/stats/LineGraph.tsx
@@ -14,32 +14,15 @@ interface LineGraphData {
   count: number;
 }
 
-const data = [
-  { date: new Date(2025, 0, 1), value: 632, state: 1 },
-  { date: new Date(2025, 0, 1), value: 7591, state: 2 },
-  { date: new Date(2025, 0, 1), value: 1024, state: 3 },
-  { date: new Date(2025, 0, 2), value: 5978, state: 1 },
-  { date: new Date(2025, 0, 2), value: 569, state: 2 },
-  { date: new Date(2025, 0, 2), value: 2194, state: 3 },
-  { date: new Date(2025, 0, 3), value: 4294, state: 1 },
-  { date: new Date(2025, 0, 3), value: 898, state: 2 },
-  { date: new Date(2025, 0, 3), value: 1492, state: 3 },
-  { date: new Date(2025, 0, 4), value: 3075, state: 1 },
-  { date: new Date(2025, 0, 4), value: 7817, state: 2 },
-  { date: new Date(2025, 0, 4), value: 5072, state: 3 },
-  { date: new Date(2025, 0, 5), value: 440, state: 1 },
-  { date: new Date(2025, 0, 5), value: 6191, state: 2 },
-  { date: new Date(2025, 0, 5), value: 3788, state: 3 },
-];
-
 export default function LineGraph({ stats }: { stats: LineGraphData[] }) {
   const lineChartRef = useRef(null);
+  const toolTipRef = useRef(null);
 
   useEffect(() => {
     const groupedStats = d3.group(stats, d => d.state);
-
     console.log(d3.active(lineChartRef.current));
 
+    const tooltip = d3.select(toolTipRef.current);
     // 차트를 그릴 컨테이너 생성
     const svg = d3
       .select(lineChartRef.current)
@@ -65,6 +48,14 @@ export default function LineGraph({ stats }: { stats: LineGraphData[] }) {
       .range([340, 0]);
     svg.append("g").call(d3.axisLeft(y_scale));
 
+    const focus = svg
+      .append("g")
+      .append("circle")
+      .attr("fill", "none")
+      .attr("stroke", "black")
+      .attr("r", 4)
+      .style("opacity", 0);
+
     const lineGenerator = d3
       .line<DataPoint>()
       .x(d => x_scale(d.date))
@@ -75,7 +66,7 @@ export default function LineGraph({ stats }: { stats: LineGraphData[] }) {
     const color = d3
       .scaleOrdinal<string>()
       .domain(statsKeys)
-      .range(["#000000", "#e41a1c", "#377eb8", "#4daf4a"]);
+      .range(["#000000", "#3498DB", "#FFA500", "#2ECC71"]);
 
     svg
       .selectAll(".line")
@@ -86,7 +77,80 @@ export default function LineGraph({ stats }: { stats: LineGraphData[] }) {
       .attr("stroke", d => color(d[0]))
       .attr("stroke-width", 2.5)
       .attr("d", d => lineGenerator(d[1]));
+
+    const mouseover = function () {
+      focus.style("opacity", 1);
+      tooltip.style("opacity", 1);
+    };
+
+    const mousemove = function (this: Element, event: MouseEvent) {
+      const mousePointer = d3.pointer(event, this);
+      const x0 = x_scale.invert(mousePointer[0]);
+
+      const aaa = Array.from(groupedStats)[0][1];
+      const dates = aaa.map(stat => stat.date);
+      const i = d3.bisectCenter(dates, x0);
+      const statsByArray = Array.from(groupedStats);
+      const points = statsByArray.map(stats => {
+        const dataPoint = stats[1][i];
+        return {
+          date: dataPoint.date,
+          count: dataPoint.count,
+          state: dataPoint.state,
+          y_pixel: y_scale(dataPoint.count),
+        };
+      });
+
+      const subYPixel = points.map(point =>
+        Math.abs(point.y_pixel - mousePointer[1]),
+      );
+      const index = subYPixel.indexOf(Math.min(...subYPixel));
+      const target = points[index];
+      const dateFormat = `${target.date.getFullYear()}-${target.date.getMonth()}-${target.date.getDate()}`;
+
+      focus.attr("cx", x_scale(target.date)).attr("cy", target.y_pixel);
+      tooltip
+        .html(
+          `${dateFormat} 일자에서<br/> ${target.state} 상태의 총합 : ${target.count}개`,
+        )
+        .style("left", `${x_scale(target.date) + 20}px`)
+        .style("top", `${target.y_pixel + 30}px`);
+    };
+
+    const mouseleave = function () {
+      focus.style("opacity", 0);
+      tooltip.style("opacity", 0);
+    };
+
+    svg
+      .append("rect")
+      .attr("fill", "none")
+      .style("pointer-events", "all")
+      .attr("width", 500)
+      .attr("height", 450)
+      .on("mouseover", mouseover)
+      .on("mousemove", mousemove)
+      .on("mouseleave", mouseleave);
   }, []);
 
-  return <div ref={lineChartRef}></div>;
+  return (
+    <>
+      <div style={{ position: "relative" }}>
+        <div
+          ref={toolTipRef}
+          style={{
+            position: "absolute",
+            opacity: 0,
+            background: "white",
+            padding: "10px",
+            border: "1px solid #ccc",
+            borderRadius: "5px",
+            zIndex: 100,
+            pointerEvents: "none",
+          }}
+        ></div>
+        <div ref={lineChartRef}></div>
+      </div>
+    </>
+  );
 }
