81571bbd6c65c1ba0d01c14320a13563eda67157
somn45 <somnsomn45@gmail.com>
2026-01-02 23:12:51 +0900

    refactor : LineGraphWrapper 서버 컴포넌트에서 상태에 의한 투두의 합계 목록을 데이터 그룹화를 쉽게 하도록 가공하는 과정을 유틸 함수로 분리

--- src/app/(private)/stats/LineGraphWrapper.tsx
+++ src/app/(private)/stats/LineGraphWrapper.tsx
@@ -1,43 +1,13 @@
 import LineGraph from "./LineGraph";
 import { setTodoStats } from "@/apis/setTodoStats";
 import { getTodoStats } from "@/apis/getTodoStats";
-import { ILineGraphData } from "@/types/schema";
+import { distributeByDate } from "./_utils/distributeByDate";
 
 export default async function LineGraphWrapper({ userid }: { userid: string }) {
   await setTodoStats(userid);
   const todoStats = await getTodoStats(userid);
 
   // [date, state, count]
-  let lineGraphData: ILineGraphData[] = [];
-  todoStats.forEach(stat => {
-    const totalStat = {
-      date: new Date(stat._id),
-      state: "총합",
-      count: stat.totalCount,
-    };
-    const todoStateStat = {
-      date: new Date(stat._id),
-      state: "할 일",
-      count: stat.todoStateCount,
-    };
-    const doingStateStat = {
-      date: new Date(stat._id),
-      state: "진행 중",
-      count: stat.doingStateCount,
-    };
-    const doneStateStat = {
-      date: new Date(stat._id),
-      state: "완료",
-      count: stat.doneStateCount,
-    };
-    lineGraphData = [
-      ...lineGraphData,
-      totalStat,
-      todoStateStat,
-      doingStateStat,
-      doneStateStat,
-    ];
-  });
-
+  const lineGraphData = distributeByDate(todoStats);
   return <LineGraph stats={lineGraphData} />;
 }
