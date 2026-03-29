0b1c65145f68a17da7ca4f4b060037689c5cde01
somn45 <somnsomn45@gmail.com>
2026-03-29 15:31:19 +0900

    build : 번들 사이즈 측정을 위해 @next/bundle-analyzer 추가

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import withBundleAnalyzer from "@next/bundle-analyzer";
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -13,4 +14,8 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+const bandleAnalyzer = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+});
+
+export default bandleAnalyzer(nextConfig);
