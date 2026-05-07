a0e5c071fd67a92292446d7548122fdc9e8a8529
somn45 <somnsomn45@gmail.com>
2026-05-07 22:06:07 +0900

    feat : 새 NextTodoChart favicon 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,9 @@ export const metadata: Metadata = {
   },
   description:
     "D3.js를 사용하여 투두리스트를 계획하고 차트로 현황을 추적합니다.",
+  icons: {
+    icon: "./NextTodoChart_favicon.png",
+  },
 };
 
 export default function RootLayout({
@@ -19,6 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <link rel="icon" href="/NextTodoChart_favicon.ico" sizes="any" />
       <body className="border-b-1st-dark m-0 flex flex-col-reverse rounded-none md:flex-col">
         <section>{modal}</section>
         {children}
